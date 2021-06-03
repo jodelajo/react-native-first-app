31209b12e730e468b3881eed5468c1f4aa80e195
jodelajo <jodelajo@gmail.com>
2021-06-03 17:57:58 +0200

    Modal and cnacel button

--- App.js
+++ App.js
@@ -1,45 +1,55 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Button } from "react-native";
 
-import GoalItem from './components/GoalItem'
-import GoalInput from './components/GoalInput'
-
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
   console.log(courseGoals);
 
   function addGoalHandler(goalTitle) {
     setCourseGoals((currentGoals) => [
-      ...currentGoals, 
-      { id: Math.random().toString(), value: goalTitle}
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle },
     ]);
+    setIsAddMode(false);
   }
 
-  function removeGoalHandler(goalId){
-    setCourseGoals(currentGoals => {
-      return currentGoals.filter((goal) => goal.id !== goalId)
-    })
+  function removeGoalHandler(goalId) {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
+  function cancelGoalAdditionHandler() {
+    setIsAddMode(false)
   }
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler}/>
-      <FlatList 
-      keyExtractor={(item, index) => item.id}
-      data={courseGoals} 
-      renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} itemData={itemData}/>}
-          />     
+      <Button title="Add new goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            itemData={itemData}
+          />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
+    marginTop: 20,
     padding: 50,
   },
- 
-  
 });
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,37 +1,46 @@
-import React, { useState} from 'react'
-import { View, TextInput, Button, StyleSheet} from 'react-native'
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
 
-function GoalInput({onAddGoal}) {
-    const [enteredGoal, setEnteredGoal] = useState("");
+function GoalInput({ onAddGoal, visible, onCancel }) {
+  const [enteredGoal, setEnteredGoal] = useState("");
 
-    function goalInputHandler(enteredText) {
-        setEnteredGoal(enteredText);
-      }
-    
-    return (
-    <View style={styles.inputContainer}>
-    <TextInput
-      placeholder="Course goal"
-      style={styles.textInput}
-      onChangeText={goalInputHandler}
-      value={enteredGoal}
-    />
-    <Button title="add" onPress={()=> onAddGoal(enteredGoal)} />
-  </View>
-  )
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
+function addGoalHandler(){
+    onAddGoal(enteredGoal);
+    setEnteredGoal('')
+}
+
+  return (
+    <Modal visible={visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="cancel" color="red" onPress={onCancel}/>
+        <Button title="add" onPress={addGoalHandler} />
+      </View>
+    </Modal>
+  );
 }
 const styles = StyleSheet.create({
-    inputContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-      },
-      textInput: {
-        borderColor: "black",
-        borderWidth: 1,
-        padding: 10,
-        width: "80%",
-      },
-})
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+    marginBottom: 10,
+  },
+});
 
-export default GoalInput
+export default GoalInput;
